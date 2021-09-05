d3542a84c0026e3e42a807905ad5748e1a300509
marcin2x4 <marcin.szpot@gmail.com>
2021-09-06 00:05:34 +0200

    code clean-up

--- xml_2_snowflake.py
+++ xml_2_snowflake.py
@@ -5,19 +5,15 @@ import os
 import snowflake.connector
 from snowflake.connector.pandas_tools import write_pandas
 
-#TEST DO GITA
-#DRUGA LINIA
-
-#nazwa pliku do zmiennej `source_file_old_name`
+#store filename as variable `source_file_old_name`
 source_file = []
 for file_name in os.listdir('<DIR>'):
     if fnmatch.fnmatch(file_name, 'Rejestr*.xml'):
         source_file.append(file_name)
 source_file_old_name = str(source_file[0])
 
-#nowa nazwa pliku, `source_file_new_name` zostanie użyta po załadowaniu do bazy do metody os.rename
+#`source_file_old_name` will be used in os.rename once db is loaded
 source_file_new_name = str('old_{}'.format(source_file[0]))
-
 
 file_path = os.path.join('<DIR>', source_file_old_name)
 #print(file_path)
@@ -32,16 +28,15 @@ for key, value in name_space.items():
     element_tree.register_namespace(key, value)
 #print(name_space)
 
-#wyciągamy pole 'Apteki.stanNaDzien' oraz wartość do osobnych zmiennych
+#'Apteki.stanNaDzien' field and values are moved to seperate variables
 data_frame_root = [{**{f"{d.tag.split('}')[1]}_{k}":v for k,v in d.items()}}
  for d in root.findall(".", name_space)]
 report_date_attr = list(data_frame_root[0].keys())[0]
 report_date = list(data_frame_root[0].values())[0]
 
-#parsowanie: oparte o namespace `  '': 'http://rejestrymedyczne.csioz.gov.pl/ra/eksport-danych-v1.0' `
-#dlatego w ścieżkach do node'ów nie ma klucza bo jest on ''
-#transformacja do dataframe, oparta o ** oraz interpolację stringów f'{} z listami i słownikami
-#wybór tylko kilku kolumn, udało się dodać prefix do każdego z child node, na razie bez root
+#parsing: based on namespace `  '': 'http://rejestrymedyczne.csioz.gov.pl/ra/eksport-danych-v1.0' `
+#no key in path as it is ''
+#transform to dataframe, based on ** and f'{} with some lists and dicts
 
 data_frame = pandas.DataFrame([{**{f"{d.tag.split('}')[1]}_{k}":v for k,v in d.items()}, 
   **{f"{co.tag.split('}')[1]}_{k}":v  for k,v in co.items()}, 
@@ -51,14 +46,12 @@ data_frame = pandas.DataFrame([{**{f"{d.tag.split('}')[1]}_{k}":v for k,v in d.i
 ], columns = ['Apteka_id', 'Apteka_rodzaj', 'Apteka_status', 'Apteka_nazwa', 'Wlasciciel_nazwa', 'Wlasciciel_nip'])
 
 
-#dostawiamy kolumny
+#extra columns
 data_frame[report_date_attr] = report_date
 data_frame['etl_filename'] = source_file[0]  
 data_frame['etl_timestamp'] = pandas.to_datetime('now').replace(microsecond=0)
 
-##########################parse XML, koniec##########################
-
-
+##########################parse XML, end##########################
 
 #connect to snowflake #1
 snowflake_connect = snowflake.connector.connect(
@@ -74,7 +67,7 @@ try:
     print("Connection ok, Snowflake version: " + str(one_row[0]))
 
 finally:
-    #zapis do STAGE
+    #write STAGE
     write_pandas(snowflake_connect, data_frame, 'STAGE_REJESTR_APTEK', quote_identifiers=False)
     print("data loaded to STAGE_REJESTR_APTEK!")
     snowflake_connect_cursor.close()
@@ -111,14 +104,14 @@ finally:
     snowflake_connect_cursor.close()
 snowflake_connect.close()
 
-#zmienne do os.rename
+#variables to os.rename
 source_file_path = os.path.join('<DIR>', source_file_old_name)
 source_file_path_renamed = os.path.join('<DIR>', source_file_new_name)
 
-#zmiana nazwy pliku
+#sourcefile re-name
 os.rename(source_file_path, source_file_path_renamed)
 print('source file renamed')
 
-#zapis do pliku
+#save to file
 #print(data_frame)
 #data_frame.to_csv('<DIR>/Filename.csv')                                
